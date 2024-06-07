40f566c9468cc58f1eedd5881812d0a1c89184c9
colebrookson <cole.brookson@gmail.com>
2024-06-07 14:39:01 -0400

    iucn data ignored

--- Figure 1/00_virus_diversity.R
+++ Figure 1/00_virus_diversity.R
@@ -6,7 +6,59 @@
 library(here)
 library(ggplot2)
 library(vroom)
+library(fasterize)
+library(magrittr)
+library(dplyr)
+library(sf)
 
 # pull updated data
-vir <- vroom::vroom("https://viralemergence.github.io/virion/Virion.csv.gz")
-str(vir)
+outdated <- FALSE
+if (outdated) {
+    vir <- vroom::vroom(
+        "https://github.com/viralemergence/virion/blob/main/Virion/Virion.csv.gz"
+    )
+    vroom::vroom_write(
+        vir,
+        here::here("./data/virion/viron.csv.gz")
+    )
+}
+# read in
+vir <- vroom::vroom(here::here("./data/virion/viron.csv.gz"))
+edges <- vroom::vroom(here::here("./data/virion/Edgelist.csv"))
+iucn_shp <- sf::st_read(here::here("./data/IUCN/"))
+
+#' NOTES
+#' Task at hand - take the host-virus matrix from virion and multiply it
+#' with a rasterized set of the IUCN shapefiles (pixel-host matrix) to get the
+#' inclusive set, in each grid cell, of the number of viruses, and then add
+#' so that each virus is only counted once
+#'
+#' This will let us create a map of maximum potential number of viruses that
+#' could be in each grid cell based on host community
+#'
+#' CAVEATS
+#' 1. This is subject to weird sampling viruses
+#' 2. There are IUCN/Virion matching processes involved here but this can be
+#'    done with code Dan Becker has written
+
+# virion manipulation ==========================================================
+
+# keep out the records we don't like
+good_taxons <- vir[which(vir$VirusNCBIResolved != FALSE &
+    vir$HostNCBIResolved != FALSE & vir$HostFlagID != TRUE), ]
+# keep only the edges that are represented here
+edges <- edges[which(
+    edges$HostTaxID %in% good_taxons$HostTaxID,
+    edges$VirusTaxID %in% good_taxons$VirusTaxID
+), ]
+# get rid of AssocId
+edges_complete <- edges %>%
+    dplyr::select(-AssocID) %>%
+    dplyr::mutate(edge = 1)
+# tidyr::complete(., HostTaxID, VirusTaxID)
+edges_complete$edge[which(is.na(edges_complete$edge))] <- 0
+edge_mat <- as.matrix(
+    table(edges_complete$HostTaxID, edges_complete$VirusTaxID)
+)
+
+# IUCN data manipulation =======================================================
