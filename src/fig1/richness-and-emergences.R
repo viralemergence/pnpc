5bf65dc2fee53ccfe39cc63c63fde254627f1987
Colin J. Carlson <colin.carlson@georgetown.edu>
2024-07-04 16:53:14 -0400

    can anyone else smell toast

--- src/fig1/richness-and-emergences.R
+++ src/fig1/richness-and-emergences.R
@@ -1,6 +1,12 @@
 
 library(tidyverse)
 library(sf)
+library(raster)
+library(fasterize)
+library(MetBrewer)
+library(ggnewscale)
+
+# raster
 
 clo1 <- read_csv("~/Documents/Github/clover/clover/clover_1.0_allpathogens/CLOVER_1.0_Bacteria_AssociationsFlatFile.csv")
 clo2 <- read_csv("~/Documents/Github/clover/clover/clover_1.0_allpathogens/CLOVER_1.0_HelminthProtozoaFungi_AssociationsFlatFile.csv")
@@ -19,3 +25,33 @@ iucn <- read_sf("./Data/IUCN/MAMMALS.shp")
 iucn %>%
   mutate(binomial = str_to_lower(binomial)) %>%
   filter(binomial %in% reservoirs) -> ranges
+
+mraster <- raster(iucn, res = 1 / 6)
+zraster <- fasterize(ranges, mraster, fun = "sum")
+
+# points
+
+eidr <- read_csv("./Data/recreation/eidr.csv")
+eidr %>%
+  dplyr::select(Longitude, Latitude, `Pathogen Type`) %>%
+  separate_rows(everything(), sep = ",") %>%
+  filter(!Latitude == "Not Found") %>%
+  filter(!Longitude == "Not Found") %>%
+  mutate(Latitude = as.numeric(Latitude), 
+         Longitude = as.numeric(Longitude)) -> eidr
+
+eidr$`Pathogen Type`[!(eidr$`Pathogen Type` %in% c('Bacteria', 'Virus'))] <- "Other"
+
+# plot
+
+ggplot() +
+  tidyterra::stat_spatraster(data = terra::rast(zraster)) +
+  theme_classic() + 
+  scale_fill_gradientn(
+    colors = met.brewer("Tiepolo", direction = -1)
+  ) + 
+  new_scale_fill() +
+  geom_point(data = eidr, aes(x = Longitude, y = Latitude, shape = `Pathogen Type`, fill = `Pathogen Type`), color = 'white', stroke = 0.6) +
+  scale_shape_manual(values = c(21, 22, 23)) + 
+  scale_fill_manual(values = c('darkblue','forestgreen','darkred')) + 
+  xlab(NULL) + ylab(NULL)
