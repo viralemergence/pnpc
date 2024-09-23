7bcf5140594ce7cc7e23292a875d1d037b211a20
carolinecummings <carolinecummings2018@gmail.com>
2024-09-23 13:10:09 -0500

    Update IUCN-mammal-reconciliation-manual.R

--- src/box2/IUCN-mammal-reconciliation-manual.R
+++ src/box2/IUCN-mammal-reconciliation-manual.R
@@ -1,6 +1,6 @@
 #' DESCRIPTION: IUCN mammal reconciliation manual
 #' AUTHOR: Cole Brookson, & Caroline Cummings
-#' Last Update: 19 September 2024
+#' Last Update: 23 September 2024
 
 # set up =======================================================================
 
@@ -327,7 +327,7 @@ miss=setdiff(vir$iucn,iucn$binomial) #2!
 #3
 #save csv
 setwd("~/Desktop/GitHub/pnpc/src/box2")
-write_csv(vir,"IUCN reconciliation manual.csv")
+#write_csv(vir,"IUCN reconciliation manual.csv")
 
 #4 
 #match IUCN/VIRION to Upham
